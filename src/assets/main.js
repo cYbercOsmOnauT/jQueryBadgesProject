bfc9ebc258882324087da2c42d2b5ce414fc04f2
Tekin Birduezen <mods@cybercosmonaut.de>
2016-11-12 23:55:46 +0100

    Finished everything

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,30 @@
 $(function() {
-
   // your code will go here
-
+    $.ajax({
+        url: 'https://www.codeschool.com/users/955481.json',
+        dataType: 'jsonp',
+        success: function(response) {
+            var createdHTML,
+                    badges = $('#badges');
+            if (response.hasOwnProperty('courses') && response.courses.hasOwnProperty('completed')) {
+                $.each(response.courses.completed, function (idx, completed) {
+                    createdHTML = $('<div>')
+                        .addClass('course')
+                        .append($('<h3>')
+                            .text(completed.title)
+                        )
+                        .append($('<img>')
+                            .attr('src', completed.badge)
+                        )
+                        .append($('<a>')
+                            .attr('href', completed.url)
+                            .attr('target', '_blank')
+                            .addClass('btn btn-primary')
+                            .text('See Course')
+                        );
+                    badges.append(createdHTML);
+                });
+            }
+        }
+    });
 });
